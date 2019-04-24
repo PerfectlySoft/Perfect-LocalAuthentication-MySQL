5256d3638d84e30d54231de5465e1dfb22bd8252
Rockford Wei <rocky@perfect.org>
2019-04-24 09:59:00 -0400

    Allowing Swift 5.0 to build
    by restricting the minimal swift toolchain version to 4.0

--- Package.swift
+++ Package.swift
@@ -1,20 +1,34 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "Perfect-LocalAuthentication-MySQL",
-    targets: [
-		Target(
-			name: "PerfectLocalAuthentication",
-			dependencies: []
-		)
-		],
+    name: "PerfectLocalAuthentication",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "PerfectLocalAuthentication",
+            targets: ["PerfectLocalAuthentication"]),
+    ],
     dependencies: [
-		.Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", majorVersion: 3),
-		.Package(url: "https://github.com/SwiftORM/MySQL-StORM.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Session-MySQL.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
-    	.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3)
-	]
+        // Dependencies declare other packages that this package depends on.
+		.package(url: "https://github.com/iamjono/JSONConfig.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-SMTP.git", from: "3.0.0"),
+		.package(url: "https://github.com/SwiftORM/MySQL-StORM.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Session-MySQL.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", from: "3.0.0"),
+    	.package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", from: "3.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "PerfectLocalAuthentication",
+            dependencies: ["JSONConfig", "PerfectRequestLogger", "PerfectSMTP", "MySQLStORM", "PerfectSessionMySQL", "PerfectMustache", "PerfectHTTP"]),
+        .testTarget(
+            name: "PerfectLocalAuthenticationTests",
+            dependencies: ["PerfectLocalAuthentication"]),
+    ]
 )
